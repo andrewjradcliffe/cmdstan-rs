303238e0644417af0910f114523889ce812fcd58
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-10-25 07:22:33 -0700

    Add docstring, though, value is questionable

--- src/internal_macros.rs
+++ src/internal_macros.rs
@@ -1,5 +1,7 @@
+/// Helper macro to implement builder pattern.
 macro_rules! insert_field {
     ($F:ident, $T:ident) => {
+        /// Configure the named option with the given value.
         pub fn $F(self, $F: $T) -> Self {
             let mut me = self;
             let _ = me.$F.insert($F);
