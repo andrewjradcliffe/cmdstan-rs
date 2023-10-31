061947c8a59f383641f198061a0c56fac9bc5ba9
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-10-31 14:55:03 -0700

    Revise Bernoulli example in light of changes

--- examples/bernoulli/main.rs
+++ examples/bernoulli/main.rs
@@ -1,5 +1,4 @@
 use cmdstan::argument_tree::{ArgumentTreeBuilder, Data, OutputBuilder, Random};
-use cmdstan::control::*;
 use cmdstan::sample::{HmcBuilder, Metric, NutsBuilder, SampleBuilder};
 use cmdstan::*;
 use std::path::PathBuf;
@@ -13,22 +12,9 @@ fn main() {
     path.push("examples");
     path.push("bernoulli");
 
-    let mut model_file = path.clone();
-    model_file.push("bernoulli.stan");
-
-    let mut data_file = path.clone();
-    data_file.push("bernoulli.data.json");
-    let mut output_file = path.clone();
-    output_file.push("output.csv");
-
-    let stan_program = StanProgram::from(model_file);
-
-    let workspace = Workspace {
-        model_name: "bernoulli".to_string(),
-        directory: path.to_string_lossy().to_string(),
-        stan_program,
-    };
-    println!("{:#?}", workspace.setup());
+    let model_file = path.join("bernoulli.stan");
+    let data_file = path.join("bernoulli.data.json");
+    let output_file = path.join("output.csv");
 
     // These options are mostly nonsense, but the point is that
     // one can conveniently specify them.
@@ -52,12 +38,12 @@ fn main() {
             file: data_file.to_string_lossy().to_string(),
         })
         .id(2)
-        .init("1".to_string())
+        .init("1")
         .random(Random { seed: 12345 })
         .output(
             OutputBuilder::new()
                 .sig_figs(4)
-                .file(output_file.to_string_lossy().to_string())
+                .file(output_file.to_string_lossy())
                 .build(),
         )
         .num_threads(48)
@@ -65,29 +51,36 @@ fn main() {
 
     // Of course, one need not rely on an environment variable, but
     // this makes the example as portable as can be.
-    let cmdstan_home =
+    let cmdstan =
         std::env::var("CMDSTAN_HOME").expect("CMDSTAN_HOME environment variable not set!");
-    let control = Control::new(&cmdstan_home, &workspace.model());
+    let model = CmdStanModel::new(&cmdstan, &model_file);
     // If a binary already exists, calling compile is somewhat strange, thus,
     // we check if there is a working rather than re-compiling by default.
-    if !control.executable_works().unwrap_or(false) {
-        println!("{:#?}", control.compile());
+    if !model.executable_works().unwrap_or(false) {
+        println!("{:#?}", model.compile());
     }
     // This will print both stdout and stderr from the method call;
     // typically, one may wish to log this. This crate may provide a
     // logging flag in the future.
-    println!("{:#?}", control.call_executable(&tree));
+    match model.call_executable(&tree) {
+        Ok(output) => {
+            println!("{:#?}", output.output());
 
-    println!("{:#?}", control.diagnose(&tree));
-    println!("{:#?}", control.stansummary(&tree, None));
+            println!("{:#?}", output.diagnose());
+            println!("{:#?}", output.stansummary(None));
+            println!("{:#?}", output.write_output());
 
-    let mut csv_filename = path.clone();
-    csv_filename.push("stansummary.csv");
-    let summary_opts = StanSummaryOptions {
-        autocorr: None,
-        csv_filename: Some(csv_filename.to_string_lossy().to_string()),
-        percentiles: Some(vec![5, 25, 50, 75, 95]),
-        sig_figs: Some(6),
-    };
-    println!("{:#?}", control.stansummary(&tree, Some(summary_opts)));
+            let csv_filename = path.join("stansummary.csv");
+            let summary_opts = StanSummaryOptions {
+                autocorr: None,
+                csv_filename: Some(csv_filename.to_string_lossy().to_string()),
+                percentiles: Some(vec![5, 25, 50, 75, 95]),
+                sig_figs: Some(6),
+            };
+            println!("{:#?}", output.stansummary(Some(summary_opts)));
+        }
+        Err(e) => {
+            println!("Something seems to have gone wrong...\n{:#?}", e);
+        }
+    }
 }
