51d2da0032217882858cd0969936c4a61d76ee5b
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-12-11 16:27:47 -0800

    Tighten this to type only

--- src/internal_macros.rs
+++ src/internal_macros.rs
@@ -10,7 +10,7 @@ macro_rules! insert_field {
 }
 
 macro_rules! insert_into_field {
-    ($F:ident, $U:ident) => {
+    ($F:ident, $U:ty) => {
         /// Configure the named option with the given value.
         pub fn $F<T: Into<$U>>(mut self, $F: T) -> Self {
             self.$F = Some($F.into());
