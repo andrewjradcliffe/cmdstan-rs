d06a1d78ba8fc1ba09a8e1cfb489a31619feef1b
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-12-11 18:54:23 -0800

    Update Bernoulli example

--- examples/bernoulli/main.rs
+++ examples/bernoulli/main.rs
@@ -1,9 +1,15 @@
-use cmdstan::argument_tree::{ArgumentTreeBuilder, Data, OutputBuilder, Random};
+use cmdstan::argument_tree::{ArgumentTree, Data, Output, Random};
 use cmdstan::sample::{HmcBuilder, Metric, NutsBuilder, SampleBuilder};
+use cmdstan::stansummary::StanSummaryOptions;
 use cmdstan::*;
 use std::{env, path::PathBuf};
 
 fn main() {
+    // Of course, one need not rely on an environment variable, but
+    // this makes the example as portable as can be.
+    let path = env::var("CMDSTAN").expect("CMDSTAN environment variable not set!");
+    let cmdstan = CmdStan::try_from(path.as_ref()).expect("Something went wrong with CmdStan");
+
     // Typically, one would not use the current working directory;
     // this example utilizes the current working directory so that it
     // may be run from within the repository using `cargo run
@@ -16,9 +22,14 @@ fn main() {
     let data_file = path.join("bernoulli.data.json");
     let output_file = path.join("output.csv");
 
-    // These options are mostly nonsense, but the point is that
+    let program = StanProgram::try_from(model_file.as_ref()).expect("Stan program does not exist");
+    let model = cmdstan
+        .compile::<[_; 0], &str>(&program, [])
+        .expect("Something went wrong with compilation");
+
+    // These options are intentionally verbose; the point is that
     // one can conveniently specify them.
-    let tree = ArgumentTreeBuilder::new()
+    let tree = ArgumentTree::builder()
         .method(
             SampleBuilder::new()
                 .num_samples(1234)
@@ -27,62 +38,35 @@ fn main() {
                 .thin(2)
                 .algorithm(
                     HmcBuilder::new()
-                        .engine(NutsBuilder::new().max_depth(100).build())
-                        .metric(Metric::DenseE)
-                        .build(),
+                        .engine(NutsBuilder::new().max_depth(100))
+                        .metric(Metric::DenseE),
                 )
-                .num_chains(4)
-                .build(),
+                .num_chains(4),
         )
-        .data(Data {
-            file: data_file.into(),
-        })
+        .data(Data::builder().file(data_file))
         .id(2)
         .init("1")
-        .random(Random { seed: 12345 })
+        .random(Random::builder().seed(12345))
         .output(
-            OutputBuilder::new()
+            Output::builder()
                 .sig_figs(4)
                 .file(output_file)
-                .profile_file(path.join("profile"))
-                .build(),
+                .profile_file(path.join("profile.csv")),
         )
         .num_threads(48)
         .build();
 
-    // Of course, one need not rely on an environment variable, but
-    // this makes the example as portable as can be.
-    let cmdstan = env::var("CMDSTAN_HOME").expect("CMDSTAN_HOME environment variable not set!");
-    let model = CmdStanModel::new(&cmdstan, &model_file);
-    // If a binary already exists, calling compile is somewhat strange, thus,
-    // we check if there is a working rather than re-compiling by default.
-    if !model.executable_works().unwrap_or(false) {
-        println!("{:#?}", model.compile());
-    }
-    // This will print both stdout and stderr from the method call;
-    // typically, one may wish to log this. This crate may provide a
-    // logging flag in the future.
-    match model.call_executable(&tree) {
-        Ok(output) => {
-            println!("{:#?}", output.output());
-            println!("{:#?}", output.diagnose());
-            println!("{:#?}", output.stansummary(None));
-            println!("{:#?}", output.write_output(Some(path.join("log.txt"))));
+    // Automatically logs stdout/stderr
+    let output = model.call(&tree).expect("Some problem with the executable");
 
-            let csv_filename = path.join("stansummary.csv");
-            let summary_opts = StanSummaryOptions {
-                autocorr: None,
-                csv_filename: Some(csv_filename.into()),
-                percentiles: Some(vec![5, 25, 50, 75, 95]),
-                sig_figs: Some(6),
-            };
-            println!("{:#?}", output.stansummary(Some(summary_opts)));
-            println!("{:#?}", output.output_files());
-            println!("{:#?}", output.diagnostic_files());
-            println!("{:#?}", output.profile_files());
-        }
-        Err(e) => {
-            println!("Something seems to have gone wrong...\n{:#?}", e);
-        }
-    }
+    let opts = StanSummaryOptions::builder()
+        .csv_filename(path.join("summary.csv"))
+        .percentiles([2.5, 50.0, 97.5])
+        .build();
+
+    let summary = cmdstan
+        .stansummary(&output, opts)
+        .expect("stansummary problem");
+    println!("{}", String::from_utf8_lossy(&summary.stdout));
+    println!("{:#?}", summary);
 }
