2758346a5be98788aea15890773dafc5b1172947
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-12-29 14:23:44 -0800

    Document purpose of `Builder` trait

--- src/builder.rs
+++ src/builder.rs
@@ -1,3 +1,7 @@
 pub use builder_derive::*;
 
+/// Trait for deriving builder methods on `struct`s and `enum`s.
+///
+/// For `struct`s, deriving this trait automatically derives `Default`.
+/// For `enum`s, a manual implementation of `Default` is required.
 pub(crate) trait Builder {}
